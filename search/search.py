70d891378e57511f3c1e53d488dc86fd8e4d7319
Eric S. Tellez <donsadit@gmail.com>
2023-08-03 10:05:17 -0500

    prepares the project for validation

--- search/search.py
+++ search/search.py
@@ -2,10 +2,12 @@ import argparse
 import faiss
 import h5py
 import numpy as np
+from sklearn import preprocessing
 import os
 from pathlib import Path
 from urllib.request import urlretrieve
-import time 
+import time
+
 
 def download(src, dst):
     if not os.path.exists(dst):
@@ -13,9 +15,10 @@ def download(src, dst):
         print('downloading %s -> %s...' % (src, dst))
         urlretrieve(src, dst)
 
+
 def prepare(kind, size):
     url = "https://sisap-23-challenge.s3.amazonaws.com/SISAP23-Challenge"
-    #url = "http://ingeotec.mx/~sadit/metric-datasets/LAION/SISAP23-Challenge"
+    # url = "http://ingeotec.mx/~sadit/metric-datasets/LAION/SISAP23-Challenge"
     task = {
         "query": f"{url}/public-queries-10k-{kind}.h5",
         "dataset": f"{url}/laion2B-en-{kind}-n={size}.h5",
@@ -23,6 +26,15 @@ def prepare(kind, size):
 
     for version, url in task.items():
         download(url, os.path.join("data", kind, size, f"{version}.h5"))
+
+
+def load_clip768(dbname, key):
+    with h5py.File(dbname, 'r') as f:
+        X = np.array(f[key], dtype=np.float32)
+        X = preprocessing.normalize(X, norm='l2')
+
+    return X
+
 
 def store_results(dst, algo, kind, D, I, buildtime, querytime, params, size):
     os.makedirs(Path(dst).parent, exist_ok=True)
@@ -37,33 +49,74 @@ def store_results(dst, algo, kind, D, I, buildtime, querytime, params, size):
     f.create_dataset('dists', D.shape, dtype=D.dtype)[:] = D
     f.close()
 
-def run(kind, key, size="100K", k=30):
+
+def create_hnsw(X, nsize, dim, neighborhoodsize, efConstruction):
+    starttime = time.time()
+    index = faiss.IndexHNSWFlat(dim, neighborhoodsize, faiss.METRIC_INNER_PRODUCT)
+    index.hnsw.efConstruction = efConstruction
+    index.add(X)
+    buildtime = time.time() - starttime
+    name = f"faiss HSNW d={dim} M={neighborhoodsize} efC={efConstruction}"
+    mem = 0  # avoids saving the index for now
+    return index, name, mem, buildtime
+
+
+def run_hsnw(kind, key, size, k=30):
     print("Running", kind)
-    
     prepare(kind, size)
 
-    data = np.array(h5py.File(os.path.join("data", kind, size, "dataset.h5"), "r")[key])
-    queries = np.array(h5py.File(os.path.join("data", kind, size, "query.h5"), "r")[key])
+    datafile = os.path.join("data", kind, size, "dataset.h5")
+    queriesfile = os.path.join("data", kind, size, "query.h5")
+    data = load_clip768(datafile, key)
+    queries = load_clip768(queriesfile, key)
     n, d = data.shape
+    index, name, mem, buildtime = create_hnsw(data, n, d, 32, 500)
+    assert index.is_trained
 
-    nlist = 128 # number of clusters/centroids to build the IVF from
+    for efSearch in [32, 64, 128, 256, 512]:
+        print(f"Starting HNSW search on {queries.shape} with efS={efSearch}")
+        start = time.time()
+        index.hnsw.efSearch = efSearch
+        D, I = index.search(queries, k)
+        elapsed_search = time.time() - start
+        print(f"Done searching HNSW in {elapsed_search}s.")
+
+        I = I + 1  # FAISS is 0-indexed, groundtruth is 1-indexed
+
+        identifier = f"{name} efSearch={efSearch}"
+        store_results(os.path.join("result/", kind, size, f"{identifier}.h5"), "faissHNSW", kind, D, I, buildtime, elapsed_search, identifier, size)
+
+
+def run_ivf(kind, key, size, k=30):
+    print("Running", kind)
+    prepare(kind, size)
+
+    nlist = 128  # number of clusters/centroids to build the IVF from
+    datafile = os.path.join("data", kind, size, "dataset.h5")
+    queriesfile = os.path.join("data", kind, size, "query.h5")
 
     if kind.startswith("pca"):
+        data = np.array(h5py.File(datafile, "r")[key])
+        queries = np.array(h5py.File(queriesfile, "r")[key])
+        n, d = data.shape
         index_identifier = f"IVF{nlist},Flat"
         index = faiss.index_factory(d, index_identifier)
     elif kind.startswith("hamming"):
-        index_identifier = f"BIVF{nlist},Flat" # use binary IVF index
-        d = 64 * d # one chunk contains 64 bits
+        data = np.array(h5py.File(datafile, "r")[key])
+        queries = np.array(h5py.File(queriesfile, "r")[key])
+        n, d = data.shape
+        index_identifier = f"BIVF{nlist},Flat"  # use binary IVF index
+        d = 64 * d  # one chunk contains 64 bits
         index = faiss.index_binary_factory(d, index_identifier)
         # create view to interpret original uint64 as 8 chunks of uint8
         data = np.array(data).view(dtype="uint8")
         queries = np.array(queries).view(dtype="uint8")
-    else:
-        # if kind == "clip768":
-        # convert vectors from float16 to float32 
-        # normalize vectors
-        # dot product / angle as distance (1-cosine) 
-        raise Exception(f"unsupported input type {kind}")
+    elif kind.startswith("clip768"):
+        data = load_clip768(datafile, key)
+        queries = load_clip768(queriesfile, key)
+        n, d = data.shape
+        index_identifier = f"IVF{nlist},Flat"
+        index = faiss.index_factory(d, index_identifier, faiss.METRIC_INNER_PRODUCT)
 
     print(f"Training index on {data.shape}")
     start = time.time()
@@ -81,18 +134,19 @@ def run(kind, key, size="100K", k=30):
         elapsed_search = time.time() - start
         print(f"Done searching in {elapsed_search}s.")
 
-        I = I + 1 # FAISS is 0-indexed, groundtruth is 1-indexed
+        I = I + 1  # FAISS is 0-indexed, groundtruth is 1-indexed
 
         identifier = f"index=({index_identifier}),query=(nprobe={nprobe})"
 
         store_results(os.path.join("result/", kind, size, f"{identifier}.h5"), "faissIVF", kind, D, I, elapsed_build, elapsed_search, identifier, size)
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--size",
-        default="100K"
+        default="300K"
     )
     parser.add_argument(
         "--k",
@@ -103,6 +157,8 @@ if __name__ == "__main__":
 
     assert args.size in ["100K", "300K", "10M", "30M", "100M"]
 
-    run("pca32v2", "pca32", args.size, args.k)
-    run("pca96v2", "pca96", args.size, args.k)
-    run("hammingv2", "hamming", args.size, args.k)
+    # run("pca32v2", "pca32", args.size, args.k)
+    # run("pca96v2", "pca96", args.size, args.k)
+    # run("hammingv2", "hamming", args.size, args.k)
+    run_hsnw("clip768v2", "emb", args.size, args.k)
+
